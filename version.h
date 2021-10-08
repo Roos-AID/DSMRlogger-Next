974fdd7f15152c497c38909976565a94840c88ba
Robert van den Breemen <robert@vandenbreemen.net>
2021-10-08 21:20:56 +0200

    improvement to mqttha integration for long term stats

--- version.h
+++ version.h
@@ -2,13 +2,15 @@
 #define _VERSION_MAJOR 2
 #define _VERSION_MINOR 3  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 507
+#define _VERSION_BUILD 512
+#define _VERSION_GITHASH "6987c2f"
 #define _VERSION_PRERELEASE rc5
-#define _VERSION_DATE "22-12-2020"
-#define _VERSION_TIME "00:17:47"
+#define _VERSION_DATE "20-09-2021"
+#define _VERSION_TIME "21:24:16"
 #define _SEMVER_CORE "2.3.0"
-#define _SEMVER_BUILD "2.3.0+507"
-#define _SEMVER_FULL "2.3.0-rc5+507"
-#define _SEMVER_NOBUILD "2.3.0-rc5 (22-12-2020)"
-#define _VERSION "2.3.0-rc5+507 (22-12-2020)"
+#define _SEMVER_BUILD "2.3.0+512"
+#define _SEMVER_GITHASH "2.3.0+6987c2f"
+#define _SEMVER_FULL "2.3.0-rc5+6987c2f"
+#define _SEMVER_NOBUILD "2.3.0-rc5 (20-09-2021)"
+#define _VERSION "2.3.0-rc5+6987c2f (20-09-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
